484820ebef3bb8fc6f8758e01a2ded5e9dd4e05f
UrbanUsling <115050797+UrbanUsling@users.noreply.github.com>
2023-03-30 21:02:15 +0200

    Add files via upload

--- Grupparbete/main.js
+++ Grupparbete/main.js
@@ -1,23 +1,18 @@
 let bought;
-    function buyFunction() {
-        if(document.getElementById("buy1")){
-            bought = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-            console.log("hej1");
-        }
-        else if(document.getElementById("buy2")){
-            bought = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-        }
-        else if(document.getElementById("buy3")){
-            bought = "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
-        }
-        else if(document.getElementById("buy4")){
-            bought = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
-            console.log("hej4");
-        }
-        
-        }
+function buyFunction(clickedValue) {
+    bought=clickedValue;
+   console.log(clickedValue);
+   console.log(bought);
+   if(clickedValue==1){
+    var p = document.getElementById("val"); 
+   p.innerHTML = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
+}
+   
+}
+
+    
         
     function displayImage() {
-        document.body.append(bought);
+        
     
-       }
+    }
