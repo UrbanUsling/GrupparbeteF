ab7fc32bd8d180dccb400f1e29c485ceb13d6432
Stefan Franzén <stefra90@live.se>
2023-04-03 23:39:31 +0200

    funkar nästan

--- Grupparbete/main2.js
+++ Grupparbete/main2.js
@@ -22,6 +22,16 @@ const myForm = document.getElementById('my-form');
 const names = document.getElementById('name1');
 const email = document.getElementById('email1');
 const phone = document.getElementById('phone1');
+const address = document.getElementById('address1');
+const postnumber = document.getElementById('post1');
+const locality = document.getElementById('locality1');
+
+myForm.addEventListener('submit', validateName);
+myForm.addEventListener('submit', validateEmail);
+myForm.addEventListener('submit', validatePhone);
+myForm.addEventListener('submit', validateAddress);
+myForm.addEventListener('submit', validatePost);
+myForm.addEventListener('submit', validateLocality);
 
 myForm.addEventListener('submit', onSubmit);
 
@@ -32,14 +42,80 @@ function onSubmit(e){
     localStorage.setItem("namnet", names.value);
     localStorage.setItem("emailet", email.value);
     localStorage.setItem("numret", phone.value);
+    localStorage.setItem("adressen", address.value);
+    localStorage.setItem("postnumret", postnumber.value);
+    localStorage.setItem("orten", locality.value);
     console.log(names.value);
     console.log(email.value);
     console.log(localStorage.getItem("numret"));
     console.log(localStorage.getItem("emailet"));
     console.log(localStorage.getItem("namnet"));
+    console.log(localStorage.getItem("adressen"));
+    console.log(localStorage.getItem("postnumret"));
+    console.log(localStorage.getItem("orten"));
 console.log(localStorage.getItem("chosen"));
 window.open("confirmation.html");
 //const recieve = document.createElement("recieve");
 //recieve.appendChild(document.createTextNode(localStorage.getItem("namnet")));
 //document.getElementById("recieve").innerHTML= localStorage.getItem("namnet");
 }
+
+function validateName() {
+    let x = document.getElementById("name1").textContent;
+    if (x.length<2) {
+        alert("Namnet är för kort");
+        return false;
+    } else if (x.length > 50) {
+        alert("Namnet är för långt");
+        return false;
+    }
+    }
+
+function validateEmail() {
+    let x = document.getElementById("email1").textContent;
+    if (x.length>50) {
+        alert("E-postadressen är för lång");
+        return false;
+    } else if (/@/.test(x)) {
+        return true;
+    }else
+        alert("E-postadressen måste innehålla @");
+        return false;
+    }
+
+function validatePhone() {
+    let x = document.getElementById("phone1").textContent;
+    let y = Number(x)
+    if (x.length>50) {
+        alert("Telefonnummret är för långt");
+        return false;
+    } else if (typeof y != Number) { //Funkar ej//
+        alert("Telefonnummret får bara innehålla siffor, -, och ()");
+        return false;
+    }
+    }
+
+function validateAddress() {
+    let x = document.getElementById("address1").textContent;
+    if (x.length>50) {
+        alert("Gatuadressen är för lång");
+        return false;
+    }
+    }
+
+function validatePost() {
+    let x = document.getElementById("post1");
+    x.setAttribute("pattern", "[0-9]{3} [0-9]{2}") //Funkar ej//
+    if (x>6) { //Funkar ej, onödig om ovanstående check funkar//
+        alert("Postnummret är för långt");
+        return false;
+    }
+    }
+
+function validateLocality() {
+    let x = document.getElementById("locality1").textContent;
+    if (x.length>50) {
+        alert("Ortnamnet är för långt");
+        return false;
+    }
+    }
--- Grupparbete/main3.js
+++ Grupparbete/main3.js
@@ -23,8 +23,14 @@ console.log(localStorage.getItem("namnet"))
          const recieve1 = document.getElementById("recieve1");
          const recieve2 = document.getElementById("recieve2");
          const recieve3 = document.getElementById("recieve3");
+         const recieve4 = document.getElementById("recieve4");
+         const recieve5 = document.getElementById("recieve5");
+         const recieve6 = document.getElementById("recieve6");
          recieve1.appendChild(document.createTextNode(localStorage.getItem("namnet")));
          recieve2.appendChild(document.createTextNode(`${localStorage.getItem("emailet")}`));
          recieve3.appendChild(document.createTextNode(`${localStorage.getItem("numret")}`));
+         recieve4.appendChild(document.createTextNode(`${localStorage.getItem("adressen")}`));
+         recieve5.appendChild(document.createTextNode(`${localStorage.getItem("postnumret")}`));
+         recieve6.appendChild(document.createTextNode(`${localStorage.getItem("orten")}`));
          console.log(localStorage.getItem("namnet"))
 
