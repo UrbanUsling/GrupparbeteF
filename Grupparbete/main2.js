6db98f0bbdf903c86180a4399919cc633853867f
UrbanUsling <115050797+UrbanUsling@users.noreply.github.com>
2023-04-04 17:02:09 +0200

    Add files via upload

--- Grupparbete/main2.js
+++ Grupparbete/main2.js
@@ -74,6 +74,9 @@ const address = document.getElementById('address1');
 const postnumber = document.getElementById('post1');
 const locality = document.getElementById('locality1');
 
+
+
+myForm.addEventListener('submit', onSubmit);
 myForm.addEventListener('submit', validateName);
 myForm.addEventListener('submit', validateEmail);
 myForm.addEventListener('submit', validatePhone);
@@ -81,90 +84,104 @@ myForm.addEventListener('submit', validateAddress);
 myForm.addEventListener('submit', validatePost);
 myForm.addEventListener('submit', validateLocality);
 
-myForm.addEventListener('submit', onSubmit);
-
-
-function onSubmit(e){
-    e.preventDefault();
-    console.log("hej");
-    localStorage.setItem("namnet", names.value);
-    localStorage.setItem("emailet", email.value);
-    localStorage.setItem("numret", phone.value);
-    localStorage.setItem("adressen", address.value);
-    localStorage.setItem("postnumret", postnumber.value);
-    localStorage.setItem("orten", locality.value);
-    console.log(names.value);
-    console.log(email.value);
-    console.log(localStorage.getItem("numret"));
-    console.log(localStorage.getItem("emailet"));
-    console.log(localStorage.getItem("namnet"));
-    console.log(localStorage.getItem("adressen"));
-    console.log(localStorage.getItem("postnumret"));
-    console.log(localStorage.getItem("orten"));
-console.log(localStorage.getItem("chosen"));
-window.open("confirmation.html");
-//const recieve = document.createElement("recieve");
-//recieve.appendChild(document.createTextNode(localStorage.getItem("namnet")));
-//document.getElementById("recieve").innerHTML= localStorage.getItem("namnet");
-}
-
-function validateName() {
-    let x = document.getElementById("name1");
-    console.log(x.value);
-    if (x.value.length<2) {
-        alert("Namnet är för kort");
-        return false;
-    } else if (x.value.length > 50) {
-        alert("Namnet är för långt");
-        return false;
-    }
-    }
-
-function validateEmail() {
-    let x = document.getElementById("email1").textContent;
-    if (x.length>50) {
-        alert("E-postadressen är för lång");
-        return false;
-    } else if (/@/.test(x)) {
-        return true;
-    }else
-        alert("E-postadressen måste innehålla @");
-        return false;
-    }
-
-function validatePhone() {
-    let x = document.getElementById("phone1").textContent;
-    let y = Number(x)
-    if (x.length>50) {
-        alert("Telefonnummret är för långt");
-        return false;
-    } else if (typeof y != Number) { //Funkar ej//
-        alert("Telefonnummret får bara innehålla siffor, -, och ()");
-        return false;
-    }
-    }
-
-function validateAddress() {
-    let x = document.getElementById("address1").textContent;
-    if (x.length>50) {
-        alert("Gatuadressen är för lång");
-        return false;
-    }
-    }
-
-function validatePost() {
-    let x = document.getElementById("post1");
-    x.setAttribute("pattern", "[0-9]{3} [0-9]{2}") //Funkar ej//
-    if (x>6) { //Funkar ej, onödig om ovanstående check funkar//
-        alert("Postnummret är för långt");
-        return false;
-    }
-    }
-
-function validateLocality() {
-    let x = document.getElementById("locality1").textContent;
-    if (x.length>50) {
-        alert("Ortnamnet är för långt");
-        return false;
-    }
-    }
+    function validateName() {
+      let x = document.getElementById("name1");
+      console.log(x.value);
+      if (x.value.length<2) {
+          alert("Namnet är för kort");
+          return false;
+      } else if (x.value.length > 50) {
+          alert("Namnet är för långt");
+          return false;
+      }else {return true;}
+      }
+  
+  function validateEmail() {
+      let x = document.getElementById("email1");
+      if (x.value.length>50) {
+          alert("E-postadressen är för lång");
+          return false;
+      }else if (x.value.indexOf("@")<0){
+          alert("E-postadressen måste innehålla @");
+          return false;
+      }else if (x.value.indexOf("@")==0){
+          alert("E-postadressen får ej börja med @");
+          return false;
+      }else {return true;}
+  }
+  
+  function validatePhone() {
+      let x = document.getElementById("phone1");
+      let y = /^[0-9-()]+$/;
+      if (x.value.length>50) {
+          alert("Telefonnummret är för långt");
+          return false;
+      } else if (!x.value.match(y)) { //Funkar ej//
+          alert("Telefonnummret får bara innehålla siffor, -, och ()");
+          return false;
+      }else {return true;}
+      }
+  
+  function validateAddress() {
+      let x = document.getElementById("address1");
+      if (x.value.length>50) {
+          alert("Gatuadressen är för lång");
+          return false;
+      }else {return true;}
+      }
+  
+  function validatePost() {
+      let x = document.getElementById("post1");
+      let y = /^[0-9 ]+$/;
+      x.setAttribute("pattern", "[0-9]{3}+ [0-9]{2}") //Funkar ej//
+      if (!x.value.match(y)){
+          alert("Postnummret får bara innehålla siffror")
+          return false;
+      }else if (x.value.indexOf(" ")!=3){
+          alert("Postnummret måste ha mellanslag efter 3:e siffran");
+          return false;
+      }else if (x.value.indexOf(" ")!=x.value.lastIndexOf(" ")){
+          alert("Postnummret får bara ha ett mellanslag");
+          return false;
+      }else if (x.value.length>6) { //Funkar ej, onödig om ovanstående check funkar//
+          alert("Postnummret är för långt");
+          return false;
+      }else if (x.value.length<6) { //Funkar ej, onödig om ovanstående check funkar//
+          alert("Postnummret är för kort");
+          return false;
+      }else {return true;}
+      }
+  
+  function validateLocality() {
+      let x = document.getElementById("locality1");
+      if (x.value.length>50) {
+          alert("Ortnamnet är för långt");
+          return false;
+      }else if (x.value.length<2){
+          alert("Ortnamnet är för kort");
+          return false;
+      }else {return true;}
+      }
+      function onSubmit(e){
+   
+         e.preventDefault();
+         if (validateAddress() && validateAddress() && validateLocality() && validateName() & validatePhone() && validatePost()){
+         localStorage.setItem("namnet", names.value);
+         localStorage.setItem("emailet", email.value);
+         localStorage.setItem("numret", phone.value);
+         localStorage.setItem("adressen", address.value);
+         localStorage.setItem("postnumret", postnumber.value);
+         localStorage.setItem("orten", locality.value);
+         console.log(names.value);
+         console.log(email.value);
+         console.log(localStorage.getItem("numret"));
+         console.log(localStorage.getItem("emailet"));
+         console.log(localStorage.getItem("namnet"));
+         console.log(localStorage.getItem("adressen"));
+         console.log(localStorage.getItem("postnumret"));
+         console.log(localStorage.getItem("orten"));
+         
+     console.log(localStorage.getItem("chosen"));
+     window.open("confirmation.html");
+         }
+         }
